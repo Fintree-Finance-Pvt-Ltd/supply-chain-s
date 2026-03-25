e369ee6a58e6c0a1ddef2b79a472640674df2f0f
joshirohit <joshirohit2004@gmail.com>
2026-03-25 17:34:56 +0530

    fixes for file sie

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -39,8 +39,8 @@ if (process.env.NODE_ENV === 'development') {
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 startBureauPdfCron();
 
